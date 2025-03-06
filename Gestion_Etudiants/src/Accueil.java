201a77e3e4ed378985a0b4dc03d85420d1fd5de5
marwenfray <fraymarwen@gmail.com>
2025-03-06 22:19:41 +0100

    ajout methode afficherEtudiant

--- Gestion_Etudiants/src/Accueil.java
+++ Gestion_Etudiants/src/Accueil.java
@@ -3,4 +3,7 @@ public class Accueil {
         //cette methode affiche le bienvenue au nouveau étudiants.
         System.out.println("Bienvenue sur la gestion des étudiants!");
     }
+    public static void afficherEtudiants() {
+        System.out.println("le nom de l'étudiant est:");
+    }
 }
