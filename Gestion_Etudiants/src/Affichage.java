b44fadad15de8d4d10845c3dc5755c70d82adc72
Makrem Harb <m.harb@mercure-tunisie.local>
2025-03-10 17:40:26 +0100

    affiche note

--- Gestion_Etudiants/src/Affichage.java
+++ Gestion_Etudiants/src/Affichage.java
@@ -1,6 +1,5 @@
 public class Affichage {
     public static void afficherEtudiant(String nom) {
-        System.out.println("=== Étudiant : " + nom + " ===");
-        System.out.println("afficher note :");
+        System.out.println("=== Étudiant : " + nom + " ==="); System.out.println("afficher note :");
     }
 } 
