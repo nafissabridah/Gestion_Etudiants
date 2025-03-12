351a4a4fdbb65c9ffc3d7e3003febb52c04decfe
nafissabridah <nafissa.bridah@esprit.tn>
2025-03-12 17:12:38 +0100

    créer conflitt2

--- Gestion_Etudiants/src/Main.java
+++ Gestion_Etudiants/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Message modifié depuis la branche NAFISSA !");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -12,5 +12,6 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
-    // Merger la  branche  nafissa
+    Accueil acc = new Accueil();
+
 }
